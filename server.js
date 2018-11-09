634ea84baab3c89483e7ea0f8bab2e39638fdb6a
Krystian Dziuk <dziukers@gmail.com>
2018-11-09 15:46:04 +0100

    added waking up server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('it is working!') })
+app.get('/', (req, res) => { res.json('server is ready!') })
 app.get('/ranking', (req, res) => { ranking.handleRanking(req, res, db) })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
